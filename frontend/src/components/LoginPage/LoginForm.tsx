2ce11a4a87facbe8560c54f8c4d6922625e4cf2b
Mahmoud Mousa <mahmoud.m@covergenius.com>
2021-02-28 20:20:16 +1100

    Add formik form basic usuage

--- frontend/src/components/LoginPage/LoginForm.tsx
+++ frontend/src/components/LoginPage/LoginForm.tsx
@@ -1,4 +1,12 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Alert, Box, Button, Heading, Text } from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+
+const loginSchema = Yup.object().shape({
+  username: Yup.string()
+    .required('Please enter your username')
+    .min(5, 'Username has to be at least 5 characters'),
+});
 
 export const LoginForm = (): JSX.Element => (
   <Box p="2">
@@ -8,5 +16,25 @@ export const LoginForm = (): JSX.Element => (
       </Heading>
       <Text>Login by entering information below</Text>
     </Box>
+    <Formik
+      initialValues={{
+        username: '',
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        console.log(values);
+      }}
+      validationSchema={loginSchema}
+    >
+      {({ errors, touched, isSubmitting }) => (
+        <Form>
+          <Field id="username" name="username" />
+          {touched?.username && errors?.username && <Alert>{errors.username}</Alert>}
+          <Button type="submit" isLoading={isSubmitting}>
+            Login
+          </Button>
+        </Form>
+      )}
+    </Formik>
   </Box>
 );
